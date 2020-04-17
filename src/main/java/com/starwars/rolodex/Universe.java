f60dca0c24f211d02e31e1f7b998265e351702a3
Vader <han72619@gmail.com>
2020-04-17 17:22:15 -0400

    Seeding the project

---
+++ src/main/java/com/starwars/rolodex/Universe.java
@@ -0,0 +1,13 @@
+package com.starwars.rolodex;
+
+/**
+ * The universe file containing the main method for the Star Wars Rolodex App
+ *
+ */
+public class Universe 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "A long time ago in a galaxy far, \nfar away..." );
+    }
+}
