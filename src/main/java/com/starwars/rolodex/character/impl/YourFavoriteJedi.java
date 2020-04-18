683ca42e011afdf7d15feaec27b5a012f86fed6f
Vader <han72619@gmail.com>
2020-04-17 21:31:59 -0400

    Adding the MysteriousJedi.java - Me :)

---
+++ src/main/java/com/starwars/rolodex/character/impl/YourFavoriteJedi.java
@@ -0,0 +1,37 @@
+package com.starwars.rolodex.character.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.starwars.rolodex.character.types.Jedi;
+
+/**
+ * The mysterious jedi... Aka me :)
+ */
+public class YourFavoriteJedi implements Jedi {
+
+    private List<String> attributes = new ArrayList<String>();
+
+
+    { // Default constructor
+        attributes.add("Art");
+        attributes.add("Rap Music");
+        attributes.add("Star Wars");
+        attributes.add("Nerd");
+        attributes.add("Deep Thinker");
+        attributes.add("Science and History");
+        attributes.add("Thoughtful");
+        attributes.add("Geology - Knows how to pronounce rocks like Gneiss and Schist");
+        attributes.add("Lifetime Learner");
+        attributes.add("Eclectic Music Taste");
+        attributes.add("Loves Collective Soul");
+        attributes.add("Loves To Dance All Night");
+        attributes.add("Shows Off The Woman I love");
+
+    }
+
+    public List<String> getAttributes() {
+        return attributes;
+    }
+
+}
--- src/main/java/com/starwars/rolodex/character/types/Character.java
+++ src/main/java/com/starwars/rolodex/character/types/Character.java
@@ -1,11 +1,16 @@
 package com.starwars.rolodex.character.types;
 
+import java.util.List;
+
 /**
- * The character interface, which will be the base super type for all Star Wars characters
+ * The character interface, which will be the base super type for all Star Wars
+ * characters
  *
  */
 public interface Character {
 
+
+    public List<String> getAttributes();
     
 
 }
